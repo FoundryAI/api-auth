222e3bea7002a8c4d92727b8f84e3e19461aa64e
Nick Gerner <hello@njgerner.com>
2017-08-29 12:22:29 -0400

    update travis yml

--- README.md
+++ README.md
@@ -19,3 +19,16 @@ app.use(apiAuth({
 
 ### Access Data
 If authorization is successful any data returned from your auth endpoint will be attached to the request and made available via `req.auth`
+```js
+app.use(apiAuth({
+  authEndpoint: 'https://auth.myapp.com/v1/oauth2/authorize'
+}));
+
+app.get('/test', (req, res, next) => {
+  console.log(req.auth)
+  // { 
+  //   userId: 'dfce809c-9245-551d-9312-ed3551e6bebb', 
+  //   scope: '*' 
+  // }
+})
+```
--- test/api-auth.spec.ts
+++ test/api-auth.spec.ts
@@ -1,1 +1,53 @@
-// TODO - write tests
+process.env.NODE_ENV = 'test';
+
+import * as express from 'express';
+import {Express} from 'express';
+import * as _ from 'lodash';
+import * as nock from 'nock';
+import * as Chance from 'chance';
+import * as supertest from 'supertest';
+import {suite, test} from 'mocha-typescript';
+import {expect} from 'chai';
+import ApiAuth from '../lib';
+
+const chance = new Chance();
+const authInfo = {
+    userId: chance.guid(),
+    scope: '*'
+};
+const accessToken = chance.guid();
+
+@suite
+class ApiAuthSpec {
+
+    private app: Express;
+
+    before() {
+        nock('https://api.foundry.ai')
+            .get(`/v1/auth/oauth2/authorize`)
+            .query({
+                access_token: accessToken
+            })
+            .reply(200, authInfo);
+
+        this.app = express();
+    }
+
+    @test
+    use() {
+        const config = {
+            authEndpoint: 'https://api.foundry.ai/v1/auth/oauth2/authorize'
+        };
+        const middleware = ApiAuth(config);
+        expect(middleware).to.be.a('function');
+        this.app.use('/', middleware);
+        this.app.get('/test', (req, res, next) => {
+            if (_.isEqual(req['auth'], authInfo)) res.sendStatus(200);
+            else res.sendStatus(500);
+        });
+
+        const request = supertest.agent(this.app);
+        return request.get(`/test?access_token=${accessToken}`)
+        .expect(200);
+    }
+}
