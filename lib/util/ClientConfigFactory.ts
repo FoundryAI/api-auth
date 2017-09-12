f19dcbedb7b5967ba248cdc0564534dac15027eb
Nick Gerner <hello@njgerner.com>
2017-09-11 20:58:46 -0400

    add support for non https

--- lib/util/ClientConfigFactory.ts
+++ lib/util/ClientConfigFactory.ts
@@ -1,6 +1,5 @@
 import { get, defaultsDeep } from 'lodash';
 import * as http from 'http';
-import * as https from 'https';
 import * as joi from 'joi';
 const version = require('../../package.json').version;
 
@@ -25,7 +24,7 @@ export function Factory (config: ApiAuthConfiguration): ApiAuthConfiguration {
             // By default, require API SSL cert to be valid
             strictSSL: true,
             // Use an agent with keep-alive enabled to avoid performing SSL handshake per connection
-            agentClass: get(config, 'request.strictSSL', true) ? https.Agent : http.Agent,
+            agentClass: http.Agent,
             agentOptions: {
                 keepAlive: true
             },
